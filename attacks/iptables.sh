a20c1744c938916f064233b1be8d01f8b00e9062
Nicklaus McClendon <nicklaus@kulinacs.com>
2018-04-09 14:54:07 -0500

    Add documentation for firewall attack

--- attacks/iptables.sh
+++ attacks/iptables.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+# Tactic: Discovery
+# Name: System Information Discovery
+# ID: 	T1082
+# Enumerates current firewall rules, allowing an attacker to better understand the network constraints on a box
 ssh -p 2222 root@127.0.0.1 "iptables -S"
