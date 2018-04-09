04b29fc1e412797d6ef892c9aeb307145f81c6e2
Nicklaus McClendon <nicklaus@kulinacs.com>
2018-04-09 15:35:08 -0500

    Add custom alert to log MITRE Attack information

---
+++ elastalert_modules/custom_alerts.py
@@ -0,0 +1,17 @@
+from elastalert.alerts import Alerter, BasicMatchString
+
+class MITREAttack(Alerter):
+    """This rule is exclusively used to classify rules in ElasticSearch"""
+
+    required_options = set(['attack_tactic', 'attack_name', 'attack_id'])
+
+    def alert(self, matches):
+        '''Alerting does nothing, because this is solely for classification'''
+        pass
+
+    def get_info(self):
+        '''This information is logged into ElasticSearch for us to use'''
+        return {'type': 'attack',
+                'attack_tactic': self.rule['attack_tactic'],
+                'attack_name': self.rule['attack_name'],
+                'attack_id': self.rule['attack_id']}
